a754657c5047cb6f5356affa04a140af81c93184
Kaeli Moore <143522412+kaelim@users.noreply.github.com>
2025-01-23 20:56:42 -0700

    Add files via upload

---
+++ HW2/README.txt
@@ -0,0 +1,5 @@
+I found it really interesting that when I prompted chatGPT that it gave
+me two different responses that I could choose between. Overall
+the documents where simalar to mine the main difference simply being 
+formatting which wasnt super shocking. They had titles for each of thairs which I 
+though twas smart.
---
+++ HW2/script.js
@@ -0,0 +1,22 @@
+// JavaScript source code
+
+    //3 favorite websites
+    console.log("https://www.youtube.com/");
+    console.log("https://www.google.com/travel/flights?tfs=CBwQARojEgoyMDI0LTA3LTMwagcIARIDTVNPcgwIBBIIL20vMDJqNzEaIxIKMjAyNC0wOC0wN2oMCAQSCC9tLzAyajcxcgcIARIDTVNPQAFIAXABggELCP___________wGYAQE&hl=en&gl=us&curr=USD");
+    console.log("https://store.steampowered.com/");
+
+    //3 favorite games
+    console.log("Sheepy: A Short Adventure");
+    console.log("Animal Well");
+    console.log("Ogu and the Secret Forest");
+
+
+    console.log("Monet with pointalism ");
+    console.log("Marina Abramovic with performance art");
+    console.log("Banksy with street art");
+
+
+//prompt
+console.log(4 + 2 + 3);
+const animal = prompt("What is your favorite animal?");
+console.log(`Your favorite color is ${animal}!`);
